ba9d30b2013a820d9ca42ac6d67bb6bd7adafd19
Robert01530 <poppaveale@icloud.com>
2026-09-03 20:36:15 +0000

    jkdsbkjf

---
+++ Class 9/3/Homework.cpp
@@ -0,0 +1,13 @@
+// library includes
+// include <library name>
+#include<iostream>
+
+// specific objects from library
+using std::cout;
+
+// entrypoint
+int main() {
+    cout << "hello world\n";
+    return 0;
+}
+
