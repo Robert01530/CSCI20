033dec6d78f598836fb6db1e85074a139dfd5d16
Robert01530 <poppaveale@icloud.com>
2026-09-19 01:38:19 -0700

    Enhance trip readiness checks with boolean flags
    Added boolean variables for trip readiness checks.

--- BooleanLogic.cpp
+++ BooleanLogic.cpp
@@ -4,7 +4,20 @@ using namespace std;
 int main()
 {
   int fuelPercent = 40;
-  bool tiresGood= True;
-  bool 
+  bool tiresGood = true;
+  bool phoneCharged = false;
+  bool Map = true;
+  bool engineWarning = false;
+  bool enoughFuel = fuelPercent >= 25;
+  bool navigationReady = phoneCharged || map;
+  bool readyForTrip = enoughFuel && tiresGood && navigationReady && !engineWarning;
+
+  if (readyForTrip)
+  {
+    Cout << "The vehicle is ready for the road trip." << end1;
+  }
+  else
+  {
+    cout << "
   return 0;
 }
